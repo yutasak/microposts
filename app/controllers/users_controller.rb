ff7cd1614b1b3e448772a5325c4ccac0140f4bd0
yutasakurai <yuta-sakurai@united.jp>
2016-04-17 12:20:36 +0000

    edit-user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,20 @@ class UsersController < ApplicationController
     end
   end
 
+def edit
+    @user = User.find(params[:id])
+end
+  
+def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功したときの処理
+    else
+      render 'edit'
+    end
+end
+
+
   private
 
   def user_params
