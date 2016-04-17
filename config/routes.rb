d38718812789826958245aa1e4838f1eb68ae270
yutasakurai <yuta-sakurai@united.jp>
2016-04-17 14:59:40 +0000

    add followings/followers

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :users
   resources :microposts
   resources :sessions, only: [:new, :create, :destroy,:edit,:update]
+  resources :relationships, only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
