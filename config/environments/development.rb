0e74e6d91f4ca26d7f1a19a96bc43c77cc687219
yutasakurai <yuta-sakurai@united.jp>
2016-04-17 09:40:03 +0000

    add user model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-  config.web_console.whitelisted_ips = '124.34.208.85'
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -40,4 +39,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  config.web_console.whitelisted_ips = '124.34.208.85'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.web_console.whitelisted_ips = '124.34.208.85'
 end
