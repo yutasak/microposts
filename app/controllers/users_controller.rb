a38c6aeda0f0475899afac68041d576cde576178
yutasakurai <yuta-sakurai@united.jp>
2016-04-17 08:33:52 +0000

    add users model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def new
+    @users = User.new
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.web_console.whitelisted_ips = '124.34.208.85'
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
