9241426189c9589dbc7d2e098af08ca9f22ef9e5
yutasakurai <yuta-sakurai@united.jp>
2016-04-19 10:12:24 +0000

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,37 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  rescue_from StandardError, with: :dispatch_error unless Rails.env.development?
-
-  def dispatch_error(exception)
-    case exception
-    when ActiveRecord::RecordNotFound, ActionController::RoutingError
-      render_404(exception)
-    else
-      render_500(exception)
+  private
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
   end
-
-  def render_404(exception = nil)
-    if exception
-      logger.info "Rendering 404 with exception: #{exception} #{exception.message}"
-    end
-    render template: "errors/error_404", status: 404, layout: 'application'
-  end
-
-  def render_500(exception = nil)
-    if exception
-      logger.info "Rendering 500 with exception: #{exception.inspect}"
-    end
-    render template: "errors/error_500", status: 500, layout: 'application'
-  end
-
-  def routing_error
-    raise ActionController::RoutingError.new(params[:path])
-  end
-
 end
-
 
 #lass ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
-  before_action :correct_user,   only: [:edit, :update]
   
   def show # 追加
    @user = User.find(params[:id])
