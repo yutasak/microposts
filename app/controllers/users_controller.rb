dfaa0023d51494f46544719bf26295ada31885f4
yutasakurai <yuta-sakurai@united.jp>
2016-04-26 07:41:01 +0000

    microcommit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,76 @@
 class UsersController < ApplicationController
-  def show # 追加
-   @user = User.find(params[:id])
-   @microposts = @user.microposts.order(created_at: :desc)
+    before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :followings, :followers]
+    before_action :correct_user,   only: [:edit, :update]
+   
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
+    @following_users = @user.following_users.all
+    @follower_users = @user.follower_users.all    
   end
   
   def new
-     @user = User.new
-     
+    @user = User.new
   end
+  
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user # ここを修正
+      redirect_to @user
     else
       render 'new'
     end
   end
-
-def edit
-    @user = User.find(params[:id])
-end
   
-def update
+ def edit
     @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      # 更新に成功したときの処理
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+   redirect_to @user , notice: 'プロフィールを編集しました'
     else
       render 'edit'
     end
-end
+  end
 
+  def followings
+    @title = "Following"
+    @user = User.find(params[:id])
+    @following_users = @user.following_users.all
+  end
 
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @follower_users = @user.follower_users.all
+  end
+  
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :edit, :update, 
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :area, :email, :password, :password_confirmation)
   end
+  
+  # beforeフィルター
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+  
+
+  
 end
